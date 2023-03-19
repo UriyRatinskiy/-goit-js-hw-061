6f267ef9eb14147bfc576f2cb42f23fd7baea56a
UriyRatinskiy <uriy_mark@ukr.net>
2023-03-19 20:48:28 +0200

    task-10

--- js/task-08.js
+++ js/task-08.js
@@ -13,50 +13,7 @@ function handleSubmit(event) {
         alert("Please fill in all the fields!");
     }
         event.currentTarget.reset();
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const form = document.querySelector(".form");
-
-// form.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const {
-//     elements: { login, password }
-//   } = event.currentTarget;
-
-//   if (login.value === "" || password.value === "") {
-//     return console.log("Please fill in all the fields!");
-//   }
-
-//   console.log(`Login: ${login.value}, Password: ${password.value}`);
-//   event.currentTarget.reset();
-// }
